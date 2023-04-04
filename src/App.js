192dbcf223975aa50afb4591827daec290598811
Ali Rasooli <arm551379@gmail.com>
2023-04-04 15:44:44 +0430

    project added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return (list = JSON.parse(list));
+  } else {
+    return [];
+  }
+};
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditId] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  const showAlert = (show = false, msg = "", type = "") => {
+    setAlert({ show, msg, type });
+  };
+  const clearList = () => {
+    showAlert(true, "empty list", "danger");
+    setList([]);
+    setName("");
+    setIsEditing(false);
+  };
+  const removeItem = (id) => {
+    showAlert(true, "item removed", "danger");
+    setList(
+      list.filter((item) => {
+        return item.id !== id;
+      })
+    );
+  };
+  const editItem = (id) => {
+    const myItem = list.find((item) => {
+      return item.id === id;
+    });
+    setEditId(id);
+    setIsEditing(true);
+    setName(myItem.title);
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "please enter value", "danger");
+    } else if (isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      showAlert(true, "value changed", "success");
+      setName("");
+      setIsEditing(false);
+      setEditId(null);
+    } else {
+      showAlert(true, "item added to the list", "success");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert}  />}
+        <h3>todo list</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            value={name}
+            placeholder="e.g. gym"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
   );
 }
 
